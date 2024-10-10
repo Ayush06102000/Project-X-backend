2996974804362cc63c2732ceb58844a1f3ab05cd
Ayush06102000 <98150940+Ayush06102000@users.noreply.github.com>
2024-10-10 15:25:34 +0530

    Update product.js

--- routes/product.js
+++ routes/product.js
@@ -3,6 +3,13 @@ const { productModel } = require("../db");
 const productRouter = Router();
 const { userMiddleware } = require('../middleware/user');
 
+const cloudinary = require('cloudinary').v2
+
+cloudinary.config({
+    cloud_name:"dp3nccyhk",
+    api_key:"787171698372227",
+    api_secret: "MT2a1K0lismkI4PlrqZ762etCRg"
+})
 
 productRouter.get("/preview",async (req,res)=>{
     const products = await productModel.find({})
@@ -14,14 +21,19 @@ productRouter.get("/preview",async (req,res)=>{
 
 productRouter.post("/products",userMiddleware,async (req,res)=>{
     const {title,description,price,imageUrl} = req.body;
+    
+    const result = await cloudinary.uploader.upload(imageUrl)
+
+    const newimageUrl = result.url;
+
 
     const product = await productModel.create({
-        title,description,price,imageUrl,creatorId:req.userId
+        title,description,price,imageUrl:newimageUrl,creatorId:req.userId
     })
     res.json({
         message:"Item Added"
-    })
-})
+    }) 
+}) 
 
 
 
